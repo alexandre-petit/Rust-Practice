e7d62730469007c4c0b57454f04db4c0dda21a25
alexandre-petit <91869285+alexandre-petit@users.noreply.github.com>
2023-08-05 11:30:41 +0200

    Update count-the-digit.rs
    - more efficient  version
    - suppressing verification texts to avoid max buffer size error

--- count-the-digit.rs
+++ count-the-digit.rs
@@ -1,6 +1,23 @@
-//version 1: max buffer size reached
+//Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+//Square all numbers k (0 <= k <= n) between 0 and n.
+
+//Count the numbers of digits d used in the writing of all the k**2.
+
+//Implement the function taking n and d as parameters and returning this count.
+
+//Examples:
+//n = 10, d = 1 
+//the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+//We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+//The function, when given n = 25 and d = 1 as argument,
+//should return 11 since the k*k that contain the digit 1 are:
+//1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+//So there are 11 digits 1 for the squares of numbers between 0 and 25.
+//Note that 121 has twice the digit 1.
+
 fn nb_dig(n: i32, d: i32) -> i32 {
-    let mut squares = Vec::new();
     let digit: char;
     
     let d_str = format!("{d}");
@@ -12,18 +29,13 @@ fn nb_dig(n: i32, d: i32) -> i32 {
         digit = 'l';
     }
     let mut counter = 0;    
-    println!("digit: {digit}");
 
     for i in 0..n+1 {
-        squares.push(i * i);
-    }
-    
-    for square in squares.iter() {
+        let square = i * i;
         let num_str = format!("{square}");
-        println!("{square}");
         let count_digit = num_str.chars().filter(|&c| c == digit).count();
-        println!("count: {count_digit}");
         counter += count_digit;
     }
+    
     counter as i32
 }
